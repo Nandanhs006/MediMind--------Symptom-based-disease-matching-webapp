95fcf901e269df6c74d152c086a7f9523c6104a2
Nandan <157642120+Nandanhs006@users.noreply.github.com>
2026-03-25 06:23:25 +0530

    commit-8 Nandan
    phewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
 
+console.log("APP FILE LOADED");
+
 const app = express();
 
 app.use(express.json());
+
+// serve root index.html for home page - BEFORE everything else
+app.get('/', (req, res) => {
+  console.log('GET / route hit');
+  res.sendFile(path.resolve(__dirname, '../index.html'));
+});
+
+// serve static files
 app.use(express.static(path.join(__dirname, '../public')));
 
+// API routes
 const diseaseRoutes = require('./routes/diseaseRoutes');
 app.use('/api', diseaseRoutes);
 
